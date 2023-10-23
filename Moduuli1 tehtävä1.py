be0a15f7de238e0b49a86d4e7ee4c3da61f92c16
katarilp <katarilp@metropolia.fi>
2023-10-23 23:55:28 +0300

    1

---
+++ Moduuli1 tehtävä1.py
@@ -0,0 +1,8 @@
+# "Auto"-luokka, jonka ominaisuudet ovat
+# rekisteritunnus, huippunopeus, tämänhetkinen nopeu ja kuljettu matka.Kirjoita luokkaan alustaja,
+# joka asettaa ominaisuuksista kaksi ensin mainittua parametreina saatuihin arvoihin.
+# Uuden auton nopeus ja kuljetut matka on asetettava automaattisesti nollaksi.
+# Kirjoita pääohjelma, jossa luot uuden auton
+# (rekisteritunnus ABC-123, huippunopeus 142 km/h).
+# Tulosta pääohjelmassa sen jälkeen luodun auton kaikki ominaisuudet.
+
