7472546c14659e2b9ee08ade9d28f165d738bf95
katarilp <katarilp@metropolia.fi>
2023-10-24 00:22:23 +0300

    1

--- Moduuli1 tehtävä1.py
+++ Moduuli1 tehtävä1.py
@@ -1,8 +1,25 @@
 # "Auto"-luokka, jonka ominaisuudet ovat
-# rekisteritunnus, huippunopeus, tämänhetkinen nopeu ja kuljettu matka.Kirjoita luokkaan alustaja,
+# rekisteritunnus, huippunopeus, tämänhetkinen nopeus ja kuljettu matka. Kirjoita luokkaan alustaja,
 # joka asettaa ominaisuuksista kaksi ensin mainittua parametreina saatuihin arvoihin.
 # Uuden auton nopeus ja kuljetut matka on asetettava automaattisesti nollaksi.
 # Kirjoita pääohjelma, jossa luot uuden auton
 # (rekisteritunnus ABC-123, huippunopeus 142 km/h).
 # Tulosta pääohjelmassa sen jälkeen luodun auton kaikki ominaisuudet.
+
+class Auto:
+    def __init__(self, reknum, huippu):
+        self.reknum = reknum
+        self.huippu = huippu
+    nopeus = 0
+    matka = 0
+
+auto1 = Auto("ABC-123", 142)
+
+print(f"Ensimmäisen auton tiedot: "
+      f"rekisteritunnus {auto1.reknum}, "
+      f"huippunopeus: {auto1.huippu} km/h, "
+      f"tämänhetkinen nopeus: {auto1.nopeus} "
+      f"ja kuljettu matka: {auto1.matka}.")
+
+
 
