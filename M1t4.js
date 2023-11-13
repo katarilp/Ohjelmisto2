e1857164d7d3d3d849622f36c32c2a50e6d4123d
katarilp <katarilp@metropolia.fi>
2023-11-13 13:48:38 +0200

    13.11.2023

--- M1t4.js
+++ M1t4.js
@@ -1,11 +1,2 @@
 'use strict';
 
-const age8 = 15;
-if (age8 < 18 ) {
-  console.log('Olet alaikäinen.')
-}
-
-const number8 = 15;
-if (number8 % 2 == 1 && number8 > 10){
-  console.log('Syötit numeron, joka on suurempi kuin 10.')
-}
