d710da7488a6547ad8d2e4154a70c4029ece2a2e
katarilp <katarilp@metropolia.fi>
2023-11-20 08:13:11 +0200

    15.11.2023

--- M1t2.js
+++
@@ -1,7 +0,0 @@
-// Write a program that prompts for user's name and then greets the user.
-// Print the result to the HTML document: Hello, Name!
-
-'use strict';
-let name = document.querySelector('#name');
-name = prompt('Kerro nimesi: ');
-name.innerHTML()
